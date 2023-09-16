5372efa03be8846247f5c69f08a2f9cec97ce04b
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-09-16 07:51:03 +0530

    chat ui

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,13 +1,32 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
+
+// Use cors middleware and specify the allowed origins
+app.use(cors({
+    origin: '*'
+}));
+
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('New client connected');
+    socket.on('new_message', (message) => {
+        // Broadcast the message to all connected clients, excluding the sender
+        socket.broadcast.emit('broadcast_message', message);
+
+        // If you also want to broadcast back to the sender, use the line below
+        // io.emit('broadcast_message', message);
+    });
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
